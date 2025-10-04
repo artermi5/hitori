aff8662cb7f7321eec24a7eecb895ce73b96bb61
artermi5 <itstimmyj9@gmail.com>
2025-10-04 14:21:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
+global.owner = ['2347044498113'] //['234','234'] 2 owner atau lebih
+global.author = 'artermis'
 global.botname = 'Hitori Bot'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
@@ -90,3 +90,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
